33c04a5a51add97d414842770f276212d3781ead
sudipstha08 <sudipstha08@gmail.com>
2024-06-10 23:24:46 +0545

    fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBV_JHu4bcAGptBHUhnbvWqDjw9HnVuq7I",
-	authDomain: "next-firebase-71a19.firebaseapp.com",
-	projectId: "next-firebase-71a19",
-	storageBucket: "next-firebase-71a19.appspot.com",
-	messagingSenderId: "476677393196",
-	appId: "1:476677393196:web:0a83454d3dbf94da077a89",
-	measurementId: "G-4MBJ3CHM19",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);

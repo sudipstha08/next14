cdbedcdc975021b483d634f679e534aeb7a733fb
sudipstha08 <sudipstha08@gmail.com>
2024-06-11 23:04:32 +0545

    feat: add nexj route handler

---
+++ src/app/team/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+export default async function TeamsPage({ params }: { params: { id: string } }) {
+  // const team = await fetchTeam(params.id)
+  // if (!team) {
+  //   redirect('/login')
+  // }
+ 
+  // ...
+}
